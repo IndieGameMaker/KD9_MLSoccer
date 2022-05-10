42ac21e983b8a5ac0276a8367c0e75fe2348f074
Lee JaeHyun <myleje@gmail.com>
2022-05-10 11:20:28 +0900

    .

---
+++ Assets/02.Scripts/BallCtrl.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+
+public class BallCtrl : MonoBehaviour
+{
+    public Agent[] players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}

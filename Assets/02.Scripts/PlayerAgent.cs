9996019fe7a0d691a0790240fa3ef111f87b918b
Lee JaeHyun <myleje@gmail.com>
2022-05-10 09:46:04 +0900

    .

--- Assets/02.Scripts/PlayerAgent.cs
+++ Assets/02.Scripts/PlayerAgent.cs
@@ -12,6 +12,12 @@ public class PlayerAgent : Agent
 
     public Team team;
 
+    //플레이어의 초기 위치
+    public Vector3 initPosBlue = new Vector3(-5.5f, 0.5f, 0.0f);
+    public Vector3 initPosRed = new Vector3(+5.5f, 0.5f, 0.0f);
+
+    //플레이어의 초기 회전값
+
     //플레이어의 색상 변경할 머티리얼
     public Material[] materials;
 

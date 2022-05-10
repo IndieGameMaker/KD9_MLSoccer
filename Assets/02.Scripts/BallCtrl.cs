7ba5591037c88edbd8a0285ebf7492f16e7396db
Lee JaeHyun <myleje@gmail.com>
2022-05-10 11:29:29 +0900

    .

--- Assets/02.Scripts/BallCtrl.cs
+++ Assets/02.Scripts/BallCtrl.cs
@@ -7,15 +7,52 @@ public class BallCtrl : MonoBehaviour
 {
     public Agent[] players;
 
-    // Start is called before the first frame update
+    private Rigidbody rb;
+
+    [SerializeField]
+    private int blueScore;
+    [SerializeField]
+    private int redScore;
+
     void Start()
     {
-
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitBall()
     {
+        transform.localPosition = new Vector3(0, 2.0f, 0);
+        rb.velocity = rb.angularVelocity = Vector3.zero;
+    }
 
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BLUE_GOAL"))
+        {
+            // RED +1, BLUE -1
+            players[0].AddReward(-1.0f);
+            players[1].AddReward(+1.0f);
+
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+
+            InitBall();
+
+            ++redScore;
+        }
+
+        if (coll.collider.CompareTag("RED_GOAL"))
+        {
+            // RED -1, BLUE +1
+            players[0].AddReward(+1.0f);
+            players[1].AddReward(-1.0f);
+
+            players[0].EndEpisode();
+            players[1].EndEpisode();
+
+            InitBall();
+
+            ++blueScore;
+        }
     }
 }
